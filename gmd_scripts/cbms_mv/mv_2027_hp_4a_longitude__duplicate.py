8d1c143748a7e79f3b34c19f89d83dede9c45731
hme128 <h.etabag@psa.gov.ph>
2026-08-26 13:37:09 +0800

    feat: add mv_2027_hp_4a_longitude__duplicate processing algorithm to pipeline provider

---
+++ gmd_scripts/cbms_mv/mv_2027_hp_4a_longitude__duplicate.py
@@ -0,0 +1,118 @@
+import os
+import json
+from typing import Any, Optional, Dict, List
+
+from PyQt5.QtCore import QVariant
+from qgis.core import (
+    NULL,
+    QgsField,
+    QgsFields,
+    QgsFeature,
+    QgsFeatureSink,
+    QgsProcessing,
+    QgsProcessingAlgorithm,
+    QgsProcessingContext,
+    QgsProcessingException,
+    QgsProcessingFeedback,
+    QgsProcessingParameterFeatureSink,
+    QgsProcessingParameterFile,
+    QgsVectorLayer,
+    QgsGeometry,
+    QgsWkbTypes,
+    QgsCoordinateReferenceSystem,
+)
+from PyQt5.QtGui import QIcon
+try:
+    from .. import gmdhelpers
+except (ImportError, ValueError):
+    try:
+        from gmd_scripts import gmdhelpers
+    except ImportError:
+        import gmdhelpers
+
+
+
+class mv_2027_hp_4a_longitude__duplicate(QgsProcessingAlgorithm):
+
+    INPUT_DATA = "INPUT_DATA"
+    INPUT_LAYER = "INPUT_LAYER"
+    OUTPUT = "OUTPUT"
+
+    def name(self) -> str:
+        return "mv_2027_hp_4a_longitude__duplicate"
+
+    def displayName(self) -> str:
+        return "mv_2027_hp_4a_longitude__duplicate"
+
+    def group(self) -> str:
+        return "2027 CBMS"
+
+    def groupId(self) -> str:
+        return "cbms_mv"
+
+    def shortHelpString(self) -> str:
+        return (
+            "List of geotagged points with duplicate map_uuid. \n \n"
+            "The map_uuid should be unique.\n"
+        )
+
+    def initAlgorithm(self, config: Optional[Dict[str, Any]] = None):
+        
+        self.addParameter(
+            QgsProcessingParameterFile(
+                self.INPUT_DATA,
+                "INPUT_DATA (.json file)",
+                behavior=QgsProcessingParameterFile.File,
+                extension="json",
+                optional=False,
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFile(
+                self.INPUT_LAYER,
+                "INPUT_LAYER (.geojson file)",
+                behavior=QgsProcessingParameterFile.File,
+                extension="geojson",
+                optional=False,
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT,
+                "mv_2027_hp_4a_map_uuid__duplicate",
+                QgsProcessing.TypeVectorAnyGeometry,
+            )
+        )
+
+    def processAlgorithm(
+        self,
+        parameters: Dict[str, Any],
+        context: QgsProcessingContext,
+        feedback: QgsProcessingFeedback,
+    ) -> Dict[str, Any]:
+
+        geojson_data = gmdhelpers.load_cbms_geojson(self, parameters, self.INPUT_LAYER, context)
+        json_data = gmdhelpers.load_cbms_json(self, parameters, self.INPUT_DATA, context, feedback)
+
+        features = gmdhelpers.filter_geometry_validity(geojson_data, feedback)
+        features, fields = gmdhelpers.add_count(features, geojson_data.fields(), "map_uuid")
+        features = [f for f in features if f["n"] > 1]
+        features = gmdhelpers.arrange(features, "map_uuid")
+
+        return gmdhelpers.export_features_to_sink(
+            self,
+            parameters,
+            self.OUTPUT,
+            context,
+            fields,
+            geojson_data.wkbType(),
+            geojson_data.sourceCrs(),
+            features,
+            feedback,
+        )
+
+
+    def createInstance(self):
+        return self.__class__()
