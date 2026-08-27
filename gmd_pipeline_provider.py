7f3fd09e19923fbc14212dbac47f78ed087c7b8d
hme128 <h.etabag@psa.gov.ph>
2026-08-27 09:24:56 +0800

    feat: add mv_2027_hp_4c_remarks__invalid algorithm to pipeline provider

--- gmd_pipeline_provider.py
+++ gmd_pipeline_provider.py
@@ -29,6 +29,7 @@ from .gmd_scripts.cbms_mv.mv_2027_hp_4b_geom__missing import mv_2027_hp_4b_geom_
 from .gmd_scripts.cbms_mv.mv_2027_hp_4a_pos_longit__invalid import mv_2027_hp_4a_pos_longit__invalid
 from .gmd_scripts.cbms_mv.mv_2027_hp_4a_longitude__duplicate import mv_2027_hp_4a_longitude__duplicate
 from .gmd_scripts.cbms_mv.mv_2027_hp_4a_longitude__invalid import mv_2027_hp_4a_longitude__invalid
+from .gmd_scripts.cbms_mv.mv_2027_hp_4c_remarks__invalid import mv_2027_hp_4c_remarks__invalid
 
 
 #from .gmd_scripts.gsheet_csv import
@@ -71,6 +72,7 @@ class GmdPipelineProvider(QgsProcessingProvider):
         self.addAlgorithm(mv_2027_hp_4a_longitude__invalid())
         self.addAlgorithm(mv_2027_hp_4b_longitude__invalid())
         self.addAlgorithm(mv_2027_hp_4a_longitude__duplicate())
+        self.addAlgorithm(mv_2027_hp_4c_remarks__invalid())
 
     def id(self):
         return 'gmd_pipeline'
